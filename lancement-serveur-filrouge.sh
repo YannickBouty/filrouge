84bec9fcdce78395f548ff2067704d8f4dd8399b
Yannick Bouty <yannickbouty@msn.com>
2021-01-16 15:36:26 +0100

    port app

--- lancement-serveur-filrouge.sh
+++ lancement-serveur-filrouge.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 export FLASK_APP=filrouge.py
 export FLASK_ENV=development
-flask run --host=0.0.0.0 -p 443 --cert=mescertificats/certificat-hello.pem --key=mescertificats/key.pem
+flask run --host=0.0.0.0 -p 55080 --cert=mescertificats/certificat-hello.pem --key=mescertificats/key.pem
